b561101f732481db5ee3dc9cb715b4eac486a4f2
Dylan Horgan <dyhorgan@gmail.com>
2022-04-19 20:29:55 -0400

    something

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.get('/hello', (req, res) => {res.send('Hello World!')})
 app.get('/', (req, res) => {res.send('Welcome to my Final Project!')})
 profileController(app);
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
 const CONNECTION_STRING = "mongodb+srv://dhorgan:Ho.66421@cluster0.robom.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 mongoose.connect(CONNECTION_STRING);
