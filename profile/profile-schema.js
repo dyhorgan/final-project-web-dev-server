0bf640e464882509000617b101fde0d273c5db68
Dylan Horgan <dyhorgan@gmail.com>
2022-04-23 22:31:21 -0400

    trying validation method

--- profile/profile-schema.js
+++ profile/profile-schema.js
@@ -4,4 +4,10 @@ const schema = mongoose.Schema({
   password: {type: String},
   imageURL: {type: String}
 }, {collection: 'profile'});
+
+schema.path('username').validate(async (username) => {
+  const profileCount = await mongoose.models.ProfileModel.countDocuments({username});
+  return !profileCount;
+}, "Username already exists")
+
 export default schema;
