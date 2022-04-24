14071c9410f7e06323f8649fc406495d27736463
Dylan Horgan <dyhorgan@gmail.com>
2022-04-23 22:41:45 -0400

    added try catch to create

--- profile/profile-dao.js
+++ profile/profile-dao.js
@@ -2,7 +2,13 @@ import profileModel from './profile-model.js';
 export const findAllProfiles = () => {
   return profileModel.find();
 };
-export const createProfile = (profile) => profileModel.create(profile);
+export const createProfile = (profile) => {
+  try{
+    profileModel.create(profile)
+  }catch(err){
+    console.log(err);
+  }
+};
 export const deleteProfile = (pid) => profileModel.deleteOne({_id: pid});
 export const updateProfile = (pid, profile) => profileModel.updateOne({_id: pid}, {$set: profile})
 
