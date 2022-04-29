eda6c5dcd54e37d402bf4648610d0ee4da4389aa
Dylan Horgan <dyhorgan@gmail.com>
2022-04-29 18:36:19 -0400

    changed following schema

--- following/following-schema.js
+++ following/following-schema.js
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
 const schema = mongoose.Schema({
   followerId: {type: String},
-  followingId: {type: String}
+  followingId: {type: String},
+  following: {type: String},
+  follower: {type: String}
 }, {collection: 'following'});
 
 export default schema;
