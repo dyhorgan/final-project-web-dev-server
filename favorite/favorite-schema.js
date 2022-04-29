f4b9559b50189f70edecfcf68370fdbd1f2c90a1
Dylan Horgan <dyhorgan@gmail.com>
2022-04-29 15:09:18 -0400

    ssss

--- favorite/favorite-schema.js
+++ favorite/favorite-schema.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
 const schema = mongoose.Schema({
   userId: {type: String},
-  movieId: {type: String}
+  movieId: {type: String},
+  title: {type: String}
 }, {collection: 'favorite'});
 
 export default schema;
