1cefe04fa9b014291cf605a1f9447ab65589e043
Dylan Horgan <dyhorgan@gmail.com>
2022-04-29 15:09:02 -0400

    added title to review

--- review/review-schema.js
+++ review/review-schema.js
@@ -3,7 +3,8 @@ const schema = mongoose.Schema({
   text: {type: String},
   userId: {type: String},
   movieId: {type: String},
-  time: {type: Date}
+  time: {type: Date},
+  title: {type: String}
 }, {collection: 'review'});
 
 export default schema;
