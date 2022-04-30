683bfe7284dce53053efd613d679863b11404533
Dylan Horgan <dyhorgan@gmail.com>
2022-04-30 16:14:51 -0400

    fixed profile by id?

--- controllers/profile-controllers.js
+++ controllers/profile-controllers.js
@@ -59,7 +59,7 @@ export default (app) => {
  app.post('/api/profiles', createProfile);
  app.get('/api/profiles', findAllProfiles);
  app.get('/api/profile', findOneProfile);
- app.get('/api/profile/:id', findProfileById);
+ app.get('/api/profiles/:id', findProfileById);
  app.put('/api/profiles/:pid', updateProfile);
  app.delete('/api/profiles/:pid', deleteProfile);
  }
